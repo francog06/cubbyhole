1504168bf2cf06176973aba151cb34b1bd4c52ba
binou83 <125899@supinfo.com>
2014-02-18 14:48:17 +0100

    MAJ
    maj système principal igniter

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'dev';
+$db['default']['password'] = 'yFJ25T3d28d96l1';
+$db['default']['database'] = 'cubbydev';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
