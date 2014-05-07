21a636e7c94ad16ceb3e5f3a94f6ea15695a32c2
Mathieu MORICEAU <mmoriceau@ymagyn.com>
2014-05-07 11:24:28 +0200

    Task #19 - [DEVELOPPEMENT] - File & Folder Link: http://redmine.cubbyhole.name/issues/19

--- application/controllers/api/file.php
+++ application/controllers/api/file.php
@@ -9,6 +9,14 @@ class File extends REST_Controller {
 
 		$this->load->library(array('email'));
 		$this->load->helper('email');
+
+		$this->uploadConfig = [
+			'allowed_types' => '*',
+			'max_size'	=> '0',
+			'overwrite' => TRUE,
+			'encrypt_name' => FALSE,
+			'file_name' => substr(md5(time()), 15)
+		];
 	}
 
 	public function details_get($id = null) {
@@ -21,7 +29,7 @@ class File extends REST_Controller {
 			$this->response(array('error' => true, 'message' => 'file not found.'), 400);
 		}
 
-		$this->response(array('error' => false, 'file' => $file));
+		$this->response(array('error' => false, 'file' => $file), 200);
 	}
 
 	public function remove_delete($id = null) {
@@ -51,35 +59,110 @@ class File extends REST_Controller {
 			$this->response(array('error' => true, 'message' => 'user not found.'), 400);
 		}
 
-		$this->response(array('error' => false, 'files' => $user->getFiles()->toArray()));
+		$this->response(array('error' => false, 'files' => $user->getFiles()->toArray()), 200);
+	}
+
+	public function update_post($id = null) {
+		if (is_null($id)) {
+			$this->response(array('error' => true, 'message' => 'id not defined.'), 400);
+		}
+
+		$file = $this->doctrine->em->find('Entities\File', (int)$id);
+		if (is_null($file)) {
+			$this->response(array('error' => true, 'message' => 'file not found.'), 400);
+		}
+
+		if ( ($folder_id = $this->post('folder_id')) !== false ) {
+			$folder = $this->doctrine->em->find('Entities\Folder', (int)$folder_id);
+			if (is_null($folder)) {
+				$this->response(array('error' => true, 'message' => 'Folder not found.'), 400);
+			}
+			$file->setFolder($folder);
+		}
+
+		if ( ($share_id = $this->post('share_id')) !== false ) {
+			$share = $this->doctrine->em->find('Entities\Share', (int)$share_id);
+			if (is_null($share)) {
+				$this->response(array('error' => true, 'message' => 'Share not found.'), 400);
+			}
+			$file->setShare($share);
+		}
+
+		if ( ($user_id = $this->post('user_id')) !== false ) {
+			$user = $this->doctrine->em->find('Entities\User', (int)$user_id);
+			if (is_null($user)) {
+				$this->response(array('error' => true, 'message' => 'user not found.'), 400);
+			}
+			$file->setUser($user);
+		}
+
+		if ( ($is_public = $this->post('is_public')) !== false ) {
+			$file->setIsPublic( $is_public == "0" ? false : true );
+			if ($file->getIsPublic() == true) {
+				$file->setAccessKey(substr(md5(time()), 15));
+			} else {
+				$file->setAccessKey(null);
+			}
+		}
+
+		// Verify is the file is not too big for the plan
+		if (isset($_FILES['file'])) {
+			$fileSize = $_FILES['file']['size'] / (1024 * 1024);
+			$fileName = $_FILES['file']['name'];
+
+			$planHistory = $file->getUser()->getActivePlanHistory();
+			if (is_null($planHistory))
+				$this->response(array('error' => true, 'message' => 'User has no active plan.'), 400);
+
+			$plan = $planHistory->getPlan();
+			if ($fileSize > $plan->getUsableStorageSpace() ||
+				($fileSize + $file->getUser()->getStorageUsed()) > $plan->getUsableStorageSpace())
+				$this->response(array('error' => true, 'message' => 'Not enough space.'), 400);
+		}
+
+		$this->uploadConfig['upload_path'] = APPPATH . 'uploads/' . $file->getUser()->getId() . "/";
+		if (!is_dir($this->uploadConfig['upload_path'])) {
+			mkdir($this->uploadConfig['upload_path'], 0777, true);
+		}
+
+		$this->load->library('upload', $this->uploadConfig);
+		if ( $this->upload->do_upload('file')) {
+			@unlink($file->getAbsolutePath());
+			$fileData = $this->upload->data();
+
+			$file->setName($fileName);
+			$file->setSize(round($fileData['file_size'] / 1024, 2));
+			$file->setAbsolutePath($fileData['full_path']);
+		}
+
+		$file->setLastUpdateDate(new DateTime('now', new DateTimeZone('Europe/Berlin')));
+		$this->doctrine->em->merge($file);
+		$this->doctrine->em->flush($file);
+		$this->response(array('error' => false, 'file' => $file), 200);
 	}
 
 	public function add_post() {
 		$user_id = $this->mandatory_value('user_id', 'post');
 
-		$config['upload_path'] = APPPATH . 'uploads/' . $user_id . "/";
-		$config['allowed_types'] = '*';
-		$config['max_size']	= '100';
-		$config['overwrite'] = TRUE;
-		$config['encrypt_name'] = FALSE;
-		$config['file_name'] = substr(md5(time()), 15);
+		$this->uploadConfig['upload_path'] = APPPATH . 'uploads/' . $user_id . "/";
 
 		$user = $this->doctrine->em->find('Entities\User', (int)$user_id);
 		if (is_null($user)) {
 			$this->response(array('error' => true, 'message' => 'user not found.'), 400);
 		}
 
-		if (!is_dir($config['upload_path'])) {
-			mkdir($config['upload_path'], 0777, true);
+		if (!is_dir($this->uploadConfig['upload_path'])) {
+			mkdir($this->uploadConfig['upload_path'], 0777, true);
 		}
 
-		$planHistory = $user->getActivePlanHistory($user);
+		$planHistory = $user->getActivePlanHistory();
 		if (is_null($planHistory))
 			$this->response(array('error' => true, 'message' => 'User has no active plan.'), 400);
 		$plan = $planHistory->getPlan();
 
 		// Verify is the file is not too big for the plan
 		if (isset($_FILES['file'])) {
+			$fileName = $_FILES['file']['name'];
 			$fileSize = $_FILES['file']['size'] / (1024 * 1024);
 
 			if ($fileSize > $plan->getUsableStorageSpace() ||
@@ -89,7 +172,7 @@ class File extends REST_Controller {
 		else
 			$this->response(array('error' => true, 'message' => 'file not found'), 400);
 
-		$this->load->library('upload', $config);
+		$this->load->library('upload', $this->uploadConfig);
 		if ( ! $this->upload->do_upload('file')) {
 			$this->response(array('error' => true, 'message' => $this->upload->display_errors('', '')), 400);
 		} else {
@@ -97,8 +180,8 @@ class File extends REST_Controller {
 			$file = new Entities\File();
 
 			$file->setUser($user);
-			$file->setName($fileData['file_name']);
-			$file->setSize($fileData['file_size']);
+			$file->setName($fileName);
+			$file->setSize(round($fileData['file_size'] / 1024, 2));
 			$file->setAbsolutePath($fileData['full_path']);
 			$file->setCreationDate(new DateTime('now', new DateTimeZone('Europe/Berlin')));
 			$file->setLastUpdateDate(new DateTime('now', new DateTimeZone('Europe/Berlin')));
@@ -115,7 +198,7 @@ class File extends REST_Controller {
 
 			$this->doctrine->em->persist($file);
 			$this->doctrine->em->flush();
-			$this->response(array('error' => false, 'file' => $file), 400);
+			$this->response(array('error' => false, 'file' => $file), 200);
 		}
 	}
 }
--- application/models/Entities/Folder.php
+++ application/models/Entities/Folder.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Entities\Folder
  */
-class Folder
+class Folder implements \JsonSerializable
 {
     /**
      * @var integer $id
@@ -307,5 +307,30 @@ class Folder
     public function getUser()
     {
         return $this->user;
+    }
+
+    /**
+     * JSON serialize
+     * 
+     * @return public object
+     */
+    public function jsonSerialize() {
+        $excludes = ["user"];
+        $json = [];
+        foreach ($this as $key => $value) {
+            if (!in_array($key, $excludes)) {
+                if (is_object($value) && strstr(get_class($value), 'Doctrine') !== false) {
+                    $collectionJson = array();
+                    foreach ($value->getKeys() as $collectionKey) {
+                        $collectionJson[] = $value->current();
+                        $value->next();
+                    }
+                    $json[$key] = $collectionJson;
+                }
+                else
+                    $json[$key] = $value;
+            }
+        }
+        return $json;
     }
 }
