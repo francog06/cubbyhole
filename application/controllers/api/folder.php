73610626cb07e09c108b60580ddc45276f0094b7
Mathieu Moriceau <moriceau.mathieu@gmail.com>
2014-05-14 00:02:34 +0200

    Task #19 - [DEVELOPPEMENT] - File & Folder Link: http://redmine.cubbyhole.name/issues/19

--- application/controllers/api/folder.php
+++ application/controllers/api/folder.php
@@ -32,11 +32,6 @@ class Folder extends REST_Controller {
 		}
 
 		$files = $folder->getFiles()->toArray();
-		$user = $folder->getUser();
-		foreach ($files as $file) {
-			$this->doctrine->em->remove($file);
-		}
-
 		$this->doctrine->em->remove($folder);
 		$this->doctrine->em->flush();
 
@@ -59,7 +54,7 @@ class Folder extends REST_Controller {
 		$folder->setLastUpdateDate(new DateTime('now', new DateTimeZone('Europe/Berlin')));
 		$folder->setIsPublic(false);
 
-		if ( ($folder_id = $this->input->post('folder_id')) ) {
+		if ( ($folder_id = $this->input->post('folder_id')) !== false ) {
 			$parentFolder = $this->doctrine->em->find('Entities\Folder', (int)$folder_id);
 			if (is_null($parentFolder)) {
 				$this->response(array('error' => true, 'message' => 'Parent folder not found.'), 400);
@@ -71,5 +66,66 @@ class Folder extends REST_Controller {
 		$this->doctrine->em->persist($folder);
 		$this->doctrine->em->flush();
 		$this->response(array('error' => false, 'folder' => $folder), 200);
+	}
+
+	public function update_put($id = null) {
+		if (is_null($id)) {
+			$this->response(array('error' => true, 'message' => 'id not defined.'), 400);
+		}
+
+		$folder = $this->doctrine->em->find('Entities\Folder', (int)$id);
+		if (is_null($folder)) {
+			$this->response(array('error' => true, 'message' => 'folder not found.'), 400);
+		}
+
+		if ( ($name = $this->put('name')) !== false )
+			$folder->setName($name);
+
+		if ( ($folder_id = $this->put('folder_id')) !== false) {
+			if ((int)$folder_id == $folder->getId())
+				$this->response(array('error' => true, 'message' => "You can't move a folder into himself"), 400);
+
+			$parentFolder = $this->doctrine->em->find('Entities\Folder', (int)$folder_id);
+			if (is_null($parentFolder)) {
+				$this->response(array('error' => true, 'message' => 'Parent folder not found.'), 400);
+			}
+
+			$folder->setParent($parentFolder);
+		}
+
+		if ( ($is_public = $this->put('is_public')) !== false ) {
+			$folder->setIsPublic( $is_public == "0" ? false : true );
+			if ($folder->getIsPublic() == true) {
+				$folder->setAccessKey(substr(md5(time()), 15));
+			} else {
+				$folder->setAccessKey(null);
+			}
+		}
+
+		$folder->setLastUpdateDate(new DateTime('now', new DateTimeZone('Europe/Berlin')));
+		$this->doctrine->em->merge($folder);
+		$this->doctrine->em->flush();
+
+		$this->response(array('error' => false, 'folder' => $folder), 200);
+	}
+
+	public function user_root_get($user_id = null) {
+		$user = $this->doctrine->em->find('Entities\User', (int)$user_id);
+		if (is_null($user)) {
+			$this->response(array('error' => true, 'message' => 'user not found.'), 400);
+		}
+
+		$folders = $user->getFolders()->filter(function($_) {
+            return ($_->getParent() == null);
+        });
+
+        $files = $user->getFiles()->filter(function($_) {
+           	return ($_->getFolder() == null);
+        });
+        $this->response(array('error' => false, 'folders' => $folders->toArray(), 'files' => $files->toArray()), 200);
+	}
+
+	public function stats_get() {
+		
 	}
 }
