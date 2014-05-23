fe1692b1dad6e4930e328af5600edbffd2269b31
franco_g <guillaume.francois@epitech.eu>
2014-05-23 15:23:24 +0200

    Task #30 [DEVELOPMENT] SHARE Link: http://redmine.cubbyhole.name/issues/30

---
+++ application/controllers/api/share.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once APPPATH . '/libraries/REST_Controller.php';
+
+class Share extends REST_Controller {
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index_get() {
+
+    }
+
+    public function details_get($id = null) {
+
+    }
+
+    public function create_post() {
+
+    }
+
+    public function update_put($id = null) {
+
+    }
+
+    public function delete_delete($id = null) {
+
+    }
+
+    public function stats_get() {
+
+    }
+}
