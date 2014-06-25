d0308926037f974d0994a735d57ce3d257922e34
binou83 <125899@supinfo.com>
2014-06-25 12:53:24 +0200

    no message

--- application/views/partial/front/telecharger.php
+++ application/views/partial/front/telecharger.php
@@ -7,21 +7,21 @@
           <h4>Web Client</h4>
           <span class="text-muted">Gérer votre compte en ligne, depuis n'importe quel ordinateur.</span>
           <br><br>
-          <button class="btn btn-info"><a href="/login/">Accéder à mon compte</a></button>
+          <a href="/login/"><button class="btn btn-info">Accéder à mon compte</button></a>
         </div>
         <div class="col-sm-3 placeholder" style="margin-right:30px;">
           <img src="/assets/img/Front/appli.png" alt="200x200" height="185px" style="border-radius:100px;">
           <h4>Application mobile</h4>
           <span class="text-muted">Accédez à vos données où que vous soyez avec votre smartphone.</span>
           <br><br>
-          <button class="btn btn-info"><a href="/assets/cubbyhole.apk">Télécharger l'application</a></button>
+          <a href="/assets/cubbyhole.apk"><button class="btn btn-info">Télécharger l'application</button></a>
         </div>
         <div class="col-sm-3 placeholder" style="margin-right:30px;">
           <img src="/assets/img/Front/windows.jpg" alt="200x200" height="185px" style="border-radius:100px;">
           <h4>Client Windows</h4>
           <span class="text-muted">Synchronisez vos données directement depuis votre PC.</span>
           <br><br>
-          <button class="btn btn-info"><a href="/assets/cubbyhole.exe">Télécharger Cubbyhole</button>
+          <a href="/assets/cubbyhole.exe"><button class="btn btn-info">Télécharger Cubbyhole</button></a>
         </div>
       </div>
 </div>
